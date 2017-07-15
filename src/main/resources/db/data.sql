f09b6403bf462cfe78bfe77b1acb5abf2b18e584
rockli0724 <819645542@qq.com>
2017-07-16 02:00:12 +0800

    提交配置文件

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,6 @@
+insert into users (username, password, enabled) values ('waylau', '123456', true);
+insert into users (username, password, enabled) values ('admin', '123', true);
+
+insert into authorities (username, authority) values ('waylau', 'ROLE_USER');
+insert into authorities (username, authority) values ('admin', 'ROLE_USER');
+insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
