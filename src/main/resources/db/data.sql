a5a49326756860d61e9fdd1e0fe5697aa599fcf1
rockli0724 <819645542@qq.com>
2017-07-16 02:01:31 +0800

    增加角色表

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -4,3 +4,4 @@ insert into users (username, password, enabled) values ('admin', '123', true);
 insert into authorities (username, authority) values ('waylau', 'ROLE_USER');
 insert into authorities (username, authority) values ('admin', 'ROLE_USER');
 insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
+
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -8,3 +8,7 @@ create table authorities (
   username varchar(256),
   authority varchar(256)
 );
+create table role (
+  id varchar(256),
+  name varchar(256)
+);
